69ded8bedb6f0bc2ccd83d61bdeb89f6d4b583c1
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2020-11-13 15:40:09 -0600

    ControlChat

--- Chat/src/chat/control/ControlChat.java
+++ Chat/src/chat/control/ControlChat.java
@@ -5,10 +5,69 @@
  */
 package chat.control;
 
+import chat.entidades.Modelo;
+import chat.entidades.Usuario;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Observer;
+import javax.xml.bind.JAXBException;
 /**
  *
  * @author Jose David
  */
 public class ControlChat {
     
+    public ControlChat(Modelo datos) {
+        this.datos = datos;
+    }
+
+    public ControlChat() {
+        this(new Modelo());
+    }
+
+    public void registrar(Observer obs) {
+        datos.addObserver(obs);
+    }
+
+    public void importar(File entrada) throws FileNotFoundException, JAXBException {
+        datos.importar(entrada);
+    }
+
+    public void exportar(File salida) throws FileNotFoundException, JAXBException {
+        datos.exportar(salida);
+    }
+
+    public void cerrarAplicacion() {
+        System.out.println("Finalizando aplicación..");
+        System.exit(0);
+    }
+
+    public Usuario recuperar(String nombreUsuario) {
+        return datos.recuperar(nombreUsuario);
+    }
+
+    public void agregar(Usuario p) {
+        datos.agregar(p);
+    }
+
+    public void actualizar(Usuario p) {
+        datos.actualizar(p);
+    }
+
+    public void eliminar(String id) {
+        datos.eliminar(id);
+    }
+
+    public Modelo obtenerModelo() {
+        return datos;
+    }
+    
+    public boolean verificarDatosUsuario(String nombreUsuario, String clave){
+        Usuario usuario = this.recuperar(nombreUsuario);
+        if (usuario != null)
+            return usuario.getClave().equals(clave);
+        return false;
+    }
+    
+    private Modelo datos;
 }
