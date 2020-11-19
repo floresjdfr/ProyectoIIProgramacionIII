2e12737ab508610200ef4c8e4201fb1c9a5910da
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2020-11-19 11:20:26 -0600

    sql script update

--- cargar_datos_usuarios.sql
+++ cargar_datos_usuarios.sql
@@ -2,7 +2,7 @@ USE `bd_chat`;
 
 DELETE FROM `bd_chat`.`usuario`;
 INSERT INTO  `bd_chat`.`usuario`
-	(nombre, nombre_completo, clave, ultimoAccesso)
+	(nombre, nombre_completo, clave, ultimoAcceso)
 	VALUES
 	('jdflores', 'Jose David Flores Rodriguez', '123', '2020-11-19 00:00:00'),
 	('mari12', 'Maria Alvarado', '123', '2020-11-19 00:00:00'),
